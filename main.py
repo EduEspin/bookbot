4a0f778c24056554facd614d72a3f11aabf07811
EduEspin <48257753+EduEspin@users.noreply.github.com>
2025-03-21 19:56:48 -0400

    add print report function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_character
+from stats import count_words, count_character, print_report
 
 def get_book_text():
     with open("./books/frankenstein.txt") as f:
@@ -6,7 +6,8 @@ def get_book_text():
     return file_contents
 
 def main():  
-    print(f"{count_words(get_book_text())} words found in the document")
-    print(count_character(get_book_text()))
+    #print(f"{count_words(get_book_text())} words found in the document")
+    #print(count_character(get_book_text()))
+    print_report(count_words(get_book_text()), count_character(get_book_text()))
     
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,17 @@ def count_character(book):
         else:
             character_list[c] += 1
     return character_list
+
+
+
+def print_report(words,characters):
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count ----------\nFound {words} total words")
+    print("--------- Character Count -------")
+    sorted_characters = dict(sorted(characters.items(), key=lambda x:x[1], reverse=True))
+   
+    for c, n  in sorted_characters.items():
+        string = str(c)
+        if string.isalpha():
+            print(f"{c}: {n}")
+    print("============= END ===============")    
